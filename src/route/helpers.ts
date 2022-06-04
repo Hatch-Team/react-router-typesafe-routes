2d51a8bbec4b2593f600b09753112d0e267493f5
Leonid Fenko <fenok2112@gmail.com>
2022-06-04 19:51:17 +0200

    WIP

--- src/route/helpers.ts
+++ src/route/helpers.ts
@@ -2,7 +2,7 @@
 export function createSearchParams(params: Record<string, string | string[]>): URLSearchParams {
     return new URLSearchParams(
         Object.keys(params).reduce((memo, key) => {
-            let value = params[key];
+            const value = params[key];
             return memo.concat(Array.isArray(value) ? value.map((v) => [key, v]) : [[key, value]]);
         }, [] as string[][])
     );
